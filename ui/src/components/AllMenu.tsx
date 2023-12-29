038cb4be55c588c3a6c28c05c2b8c4df8d57b554
Patrick Jakobsen <97399891+CPTRICODEV@users.noreply.github.com>
2023-12-29 19:53:02 +0100

    Almost ready for publish

--- ui/src/components/AllMenu.tsx
+++ ui/src/components/AllMenu.tsx
@@ -1,24 +1,28 @@
 "use client";
-import { data } from "@/lib/items";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { ScrollArea } from "./ui/scroll-area";
 import { nuiCallback } from "@/lib/nuiCallback";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface ItemData {
   price?: number;
   item: string;
   itemLabel: string;
+  itemType: string;
+  imageName: string;
 }
 
 const AllMenu = () => {
+  const [itemDataArray, setItemDataArray] = useState<ItemData[]>([]);
+
   useEffect(() => {
-    const messageHandler = (event: MessageEvent<ItemData>) => {
+    const messageHandler = (event: MessageEvent<ItemData[]>) => {
       const data = event.data;
-      if (data.price !== undefined) {
-        console.log("no price");
-      }
+      console.log("Received data:", data);
+
+      setItemDataArray(data);
     };
 
     window.addEventListener("message", messageHandler);
@@ -35,14 +39,14 @@ const AllMenu = () => {
   return (
     <ScrollArea className="w-full h-[590px]">
       <div className="grid grid-cols-4 gap-5">
-        {data.AllData.map((item, idx) => (
+        {itemDataArray.map((itemData, idx) => (
           <div
-            key={idx}
+            key={itemData.item}
             className="w-[200px] h-[250px] bg-[#0C0C0C]  border-2 border-[#313131] rounded-xl flex items-center justify-center"
           >
             <div className=" relative flex flex-col w-full h-full">
               <Image
-                src={item.ImageName}
+                src={`/${itemData.item}.png`}
                 alt="logo"
                 width={400}
                 height={400}
@@ -51,16 +55,15 @@ const AllMenu = () => {
               <div className="bg-[#242424]/40 backdrop-blur-sm absolute inset-0 z-1 w-full h-[100px] mt-[147px] rounded-b-xl flex flex-col">
                 <div className="flex flex-row justify-between w-full px-2 mt-4">
                   <h1 className="text-white text-sm font-semibold">
-                    {item.name}
+                    {itemData.itemLabel}
                   </h1>
-                  <h1 className="text-white text-sm">{item.effect}</h1>
                 </div>
                 <h1 className="text-white text-sm font-semibold px-2 mt-1">
-                  Pris: {item.price} DKK
+                  Pris: {itemData.price} DKK
                 </h1>
                 <div className="w-full px-2">
                   <Button
-                    onClick={() => buyItem(item.itemName)}
+                    onClick={() => buyItem(itemData.item)}
                     className="w-full bg-white text-black h-[27px] mt-2 hover:bg-white/80 hover:text-black"
                   >
                     Køb
